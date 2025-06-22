490a036f1301e4583ba6d52c8f68062ed9bb3086
rohit singh <rohit2701singh@gmail.com>
2025-06-23 02:42:02 +0530

    Login using Scrapy

---
+++ Scrapy_Advance/QuoteBookScrape/QuoteBookScrape/spiders/SiteLogin.py
@@ -0,0 +1,35 @@
+import scrapy
+from scrapy.http import FormRequest
+from scrapy.utils.response import open_in_browser
+
+
+class LoginSpider(scrapy.Spider):
+    name = 'login_quotes'
+    start_urls = ['https://quotes.toscrape.com/login']
+
+    def parse(self, response):
+        # Send a POST request with the login form
+        return FormRequest.from_response(
+            response,
+            formdata={'user name': 'admin', 'password': 'admin'},
+            callback=self.after_login
+        )
+
+    def after_login(self, response):
+        # We're now logged in if credentials are correct
+        if "Logout" in response.text:
+            self.logger.info("✅ Login successful!")
+        else:
+            self.logger.error("❌ Login failed!")
+
+        # Proceed to scrape after login
+        return response.follow("/tag/humor/", callback=self.parse_quotes)
+
+    def parse_quotes(self, response):
+        open_in_browser(response)   # open page in browser
+        # for quote in response.css("div.quote"):
+        #     yield {
+        #         "author": quote.css("small::text").get(),
+        #         "text": quote.css("span.text::text").get(),
+        #         "tags": quote.css("a.tag::text").getall(),
+        #     }
